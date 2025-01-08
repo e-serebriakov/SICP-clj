297cb3e5a6efd873ef1ab86e053082bd59935c47
Evgenii Serebriakov <eugene.serebriakov@gmail.com>
2025-01-08 12:41:03 +0000

    WIP

--- src/chapter_1/chapter_1.clj
+++ src/chapter_1/chapter_1.clj
@@ -34,5 +34,119 @@ size
 
 (sum-of-squares 3 4)
 
+(defn f [a] (sum-of-squares (+ a 1) (* a 2)))
 
+(f 5)
 
+;; Normal-order evaluation — full substitution first
+;; (f 5)
+;; (sum-of-squares (+ 5 1) (* 5 2))
+;; (+ (square (+ 5 1)) (square (* 5 2)))
+;; (+ (* (+ 5 1) (+ 5 1)) (* (* 5 2) (* 5 2)))
+;; (+ (* 6 6) (* 10 10))
+;; (+ 36 100)
+;; 136
+
+;; Applicative-order evaluation
+;; (f 5)
+;; (sum-of-squares (+ 5 1) (* 5 2)) 
+;; (sum-of-squares 6 10)
+;; (+ (sqr 6) (sqr 10))
+;; (+ (* 6 6) (* 10 10))
+;; (+ 36 100)
+;; 136
+
+(defn abs-1 [x] 
+  (cond
+    (> x 0) x
+    (< x 0) (- x)
+    (= x 0) 0
+    )
+  )
+
+(abs-1 0)
+(abs-1 1)
+(abs-1 (- 1))
+
+(defn abs-2 [x]
+  (cond
+    (< x 0) (- x)
+    :else x))
+
+(abs-2 0)
+(abs-2 1)
+(abs-2 (- 1))
+
+(defn abs-3 [x]
+  (if (< x 0) (- x) x))
+
+(abs-3 0)
+(abs-3 1)
+(abs-3 (- 1))
+
+(defn between-3-and-10 [x] (and (> x 3) (< x 10)))
+
+(between-3-and-10 3)
+(between-3-and-10 4)
+
+; Exercise 1.1
+
+; Exercise 1.2
+(/ (+ 5 4 (- 2 (- 3 (+ 6 (/ 4 5))))) (* 3 (- 6 2) (- 2 7)))
+
+; Exercise 1.3
+(defn sum-of-biggest-numbers [a b c]
+  (cond
+    (and (> a c) (> b c)) (sum-of-squares a b)
+    (and (> c a) (> b a)) (sum-of-squares b c)
+    :else (sum-of-squares a c)))
+
+(sum-of-biggest-numbers 1 2 3)
+
+; Exercise 1.4
+; Describe the logic of the following function
+(defn a-plus-abs-b [a b]
+  ((if (> b 0) + -) a b))
+; If b > -> a + b, otherwise -> a - b
+(a-plus-abs-b 1 2)
+(a-plus-abs-b 1 (- 2))
+
+; Exercise 1.5
+(defn p [] p)
+(defn test-1 [x y]
+  (if (= x 0) 0 y))
+(test-1 0 (p))
+
+; Normal-order evaluation
+; (if (= 0 0) 0 (p))
+; (if (= 0 0) 0 (p))
+; etc
+; -> infinite recursion
+
+; Applicative-order evaluation
+; (if (= 0 0) 0 (p)) 
+; 0
+; ---------
+
+(defn good-enought? [guess x]
+  (< (abs (- (sqr guess) x)) 0.001))
+
+(defn average [a b] 
+  (/ (+ a b) 2))
+
+(defn improve [guess x]
+  (average guess (/ x guess)))
+
+(defn sqrt-iter [guess x]
+  (if (good-enought? guess x)
+    guess
+    (sqrt-iter (improve guess x) x)))
+
+(defn sqrt [x]
+  (sqrt-iter 1.0 x))
+
+(sqrt 9)
+
+(sqrt (+ 100 37))
+
+; Exercise 1.6
