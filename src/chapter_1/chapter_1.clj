98b212203e960c9c332e0260ff72b7058c598592
Evgenii Serebriakov <eugene.serebriakov@gmail.com>
2025-01-15 21:52:45 +0000

    WIP

--- src/chapter_1/chapter_1.clj
+++ src/chapter_1/chapter_1.clj
@@ -135,3 +135,20 @@ size
   (iter 1 1)))
 
 (factorial2 3)
+
+(defn count-change [amount] 
+  (letfn [(first-denomination [kind-of-coins] (cond (= kind-of-coins 1) 1
+                                                    (= kind-of-coins 2) 5
+                                                    (= kind-of-coins 3) 10
+                                                    (= kind-of-coins 4) 25
+                                                    (= kind-of-coins 5) 50))
+          (cc [amount kind-of-coins] (cond
+                                       (= amount 0) 1
+                                       (or (< amount 0)
+                                           (= kind-of-coins 0)) 0
+                                       :else (+ (cc amount (- kind-of-coins 1))
+                                                (cc (- amount (first-denomination kind-of-coins))
+                                                    kind-of-coins))))]
+  (cc amount 5)))
+
+(count-change 100)
