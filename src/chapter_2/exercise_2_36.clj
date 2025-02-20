d55df1dbdc26ba409bdde048c77b01242cb0000b
Evgenii Serebriakov <eugene.serebriakov@gmail.com>
2025-02-20 21:57:42 +0000

    Exercise 2.36

---
+++ src/chapter_2/exercise_2_36.clj
@@ -0,0 +1,28 @@
+(ns chapter-2.exercise-2-36 
+  (:require
+   [chapter-2.chapter-2 :refer [accumulate]]))
+
+;; The procedure accumulate-n is similar to
+;; accumulate except that it takes as its third argument
+;; a sequence of sequences, which are all assumed to have the
+;; same number of elements. It applies the designated accumulation procedure
+;; to combine all the first elements of the
+;; sequences, all the second elements of the sequences, and so
+;; on, and returns a sequence of the results. For instance, if s
+;; is a sequence containing four sequences, ((1 2 3) (4 5 6)
+;; (7 8 9) (10 11 12)), then the value of (accumulate-n + 0 s)
+;; should be the sequence (22 26 30). Fill in the missing
+;; expressions in the following definition of accumulate-n:
+;; (define (accumulate-n op init seqs)
+;;   (if (null? (car seqs))
+;;     nil
+;;     (cons (accumulate op init ⟨??⟩)
+;;      (accumulate-n op init ⟨??⟩))))
+(defn accumulate-n [op init seqs]
+  (if (empty? (first seqs))
+    nil
+    (cons (accumulate op init (map (fn [item] (first item)) seqs))
+          (accumulate-n op init (map (fn [item] (rest item)) seqs)))))
+
+(def s (list '(1 2 3) '(4 5 6) '(7 8 9) '(10 11 12)))
+(accumulate-n + 0 s)
