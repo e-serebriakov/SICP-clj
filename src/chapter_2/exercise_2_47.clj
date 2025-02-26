c36490aa3e0425967a6cd2dead5bc129b737b613
Evgenii Serebriakov <eugene.serebriakov@gmail.com>
2025-02-26 21:45:16 +0000

    Exercise 2.47

---
+++ src/chapter_2/exercise_2_47.clj
@@ -0,0 +1,32 @@
+(ns chapter-2.exercise-2-47)
+
+;; Here are two possible constructors for frames:
+;; (define (make-frame origin edge1 edge2)
+;;   (list origin edge1 edge2))
+;; (define (make-frame origin edge1 edge2)
+;;   (cons origin (cons edge1 edge2)))
+;; For each constructor supply the appropriate selectors to
+;; produce an implementation for frames.
+(defn make-frame [origin edge1 edge2]
+  (list origin edge1 edge2))
+
+(def origin first)
+(def edge-1 second)
+(def edge-2 last)
+
+(def frame (make-frame 1 2 3))
+(origin frame)
+(edge-1 frame)
+(edge-2 frame)
+
+
+(defn make-frame [origin edge1 edge2]
+  (cons origin (cons edge1 edge2)))
+
+(def origin first)
+(def edge-1 (comp first rest))
+(def edge-2 (comp first rest rest))
+
+(origin frame)
+(edge-1 frame)
+(edge-2 frame)
